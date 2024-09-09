e878047e92400df855c578d69cce62f182a7f039
yasu9w <yasu9w@gmail.com>
2024-09-09 22:00:00 +0900

    Add src/index.js src/App.js

---
+++ frontend/src/App.js
@@ -0,0 +1,106 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route, Link, useLocation } from 'react-router-dom';
+import DocsMain from './docs/App';
+import PurchaseMain from './purchase/App';
+
+function Navigation() {
+    const location = useLocation();
+
+    // トップページ('/')のみにナビゲーションバーを表示
+    if (location.pathname !== '/') {
+        return null;
+    }
+
+    return (
+        <div style={styles.container}>
+            <div style={styles.content}>トップページのコンテンツ</div>
+            <nav style={styles.nav}>
+                <ul style={styles.ul}>
+                    <li style={styles.li}>
+                        <Link to="/docs" style={styles.link}>Docs</Link>
+                    </li>
+                    <li style={styles.li}>
+                        <Link to="/purchase" style={styles.link}>Purchase</Link>
+                    </li>
+                </ul>
+            </nav>
+        </div>
+    );
+}
+
+function Footer() {
+    const location = useLocation();
+
+    // トップページ('/')のみにフッターを表示
+    if (location.pathname !== '/') {
+        return null;
+    }
+
+    return (
+        <footer style={styles.footer}>
+            <p>© 2024 data2tx, Inc.</p>
+        </footer>
+    );
+}
+
+function App() {
+    return (
+        <Router>
+            <div style={styles.appContainer}>
+                <Routes>
+                    <Route path="/" element={<><Navigation /><Footer /></>} />
+                    <Route path="/docs/*" element={<DocsMain />} />
+                    <Route path="/purchase" element={<PurchaseMain />} />
+                </Routes>
+            </div>
+        </Router>
+    );
+}
+
+const styles = {
+    appContainer: {
+        display: 'flex',
+        flexDirection: 'column',
+        minHeight: '100vh',
+    },
+    container: {
+        flex: '1',
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        justifyContent: 'center',
+        textAlign: 'center',
+    },
+    content: {
+        marginBottom: '20px',
+        fontSize: '24px',
+        fontWeight: 'bold',
+    },
+    nav: {
+        marginTop: '20px',
+    },
+    ul: {
+        listStyleType: 'none',
+        padding: 0,
+    },
+    li: {
+        marginBottom: '10px',
+        textAlign: 'left',
+    },
+    link: {
+        textDecoration: 'underline', // アンダーバーを追加
+        fontSize: '20px',
+        color: '#0000EE', // リンクを青文字に変更
+        display: 'inline-block',
+        transition: 'color 0.2s ease',
+    },
+    footer: {
+        textAlign: 'center',
+        padding: '10px 0',
+        backgroundColor: '#fff',
+        marginTop: 'auto',
+        borderTop: '1px solid #eaeaea',
+    },
+};
+
+export default App;
