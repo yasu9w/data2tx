f97e9fa46fb84fd4929ce1a168ac6df8649dbb31
yasu9w <yasu9w@gmail.com>
2024-09-09 20:48:47 +0900

    frontend files

---
+++ frontend/config-overrides.js
@@ -0,0 +1,13 @@
+﻿const webpack = require('webpack');
+
+module.exports = function override(config, env) {
+    config.resolve.fallback = {
+        crypto: require.resolve('crypto-browserify'),
+        stream: require.resolve('stream-browserify'),
+        http: require.resolve('stream-http'),
+        https: require.resolve('https-browserify'),
+        zlib: require.resolve('browserify-zlib'),
+    };
+
+    return config;
+};
