ff78bf995713de91a2e8f01f3af0c5af2ff410d3
yasu9w <yasu9w@gmail.com>
2024-09-22 22:03:22 +0900

    fix: Docs/App.css

--- frontend/src/docs/index.js
+++ frontend/src/docs/index.js
@@ -4,6 +4,11 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+const meta = document.createElement('meta');
+meta.name = 'viewport';
+meta.content = 'width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no';
+document.getElementsByTagName('head')[0].appendChild(meta);
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
