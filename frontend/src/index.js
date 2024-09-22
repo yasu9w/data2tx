dd58b3a3ff7ec54fe7da0aedef799ac64c629154
yasu9w <yasu9w@gmail.com>
2024-09-22 21:55:41 +0900

    add: header toppage index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,6 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
+const meta = document.createElement('meta');
+meta.name = 'viewport';
+meta.content = 'width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no';
+document.getElementsByTagName('head')[0].appendChild(meta);
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <React.StrictMode>
