eaa5c6c01cdcd34cb18786b9f495ddc587eca375
yasu9w <yasu9w@gmail.com>
2024-09-22 15:40:23 +0900

    fix unlock phantom

--- frontend/src/upload/WalletContextProvider.js
+++ frontend/src/upload/WalletContextProvider.js
@@ -5,8 +5,7 @@ import { PhantomWalletAdapter, SolflareWalletAdapter } from '@solana/wallet-adap
 import '@solana/wallet-adapter-react-ui/styles.css';
 
 const WalletContextProvider = ({ children }) => {
-    //const wallets = [new PhantomWalletAdapter(), new SolflareWalletAdapter()];
-    const wallets = [new SolflareWalletAdapter()];
+    const wallets = [new PhantomWalletAdapter(), new SolflareWalletAdapter()];
 
     const QUICKNODE_RPC = process.env.REACT_APP_QUICKNODE_RPC;
 
