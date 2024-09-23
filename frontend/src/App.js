5d4ecaec29d4c6dbdd280314c2893b7b5c073e38
yasu9w <yasu9w@gmail.com>
2024-09-23 11:39:04 +0900

    fix: upload for PC

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Link, useLocation } from 'react-router-dom';
 import DocsMain from './docs/App';
 import PurchaseMain from './purchase/App';
 import DownloadMain from './download/App';
