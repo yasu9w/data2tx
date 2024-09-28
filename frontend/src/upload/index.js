7e9971b353b9b53e60f19d25a1767e41e83336e7
yasu9w <yasu9w@gmail.com>
2024-09-28 23:51:02 +0900

    add: viewport metatag

--- frontend/src/upload/index.js
+++ frontend/src/upload/index.js
@@ -6,6 +6,15 @@ import './index.css';
 const container = document.getElementById('root');
 const root = createRoot(container); // createRoot(container!) if you use TypeScript
 
+const addViewportMetaTag = () => {
+    const viewportMetaTag = document.createElement('meta');
+    viewportMetaTag.name = 'viewport';
+    viewportMetaTag.content = 'width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no';
+    document.head.appendChild(viewportMetaTag);
+};
+
+addViewportMetaTag();
+
 root.render(
     <React.StrictMode>
         <App />
